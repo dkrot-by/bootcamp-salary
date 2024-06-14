091af1c32a2606e61339ab728b2d19133919bc4f
dkrot-by <kdima.1983@gmail.com>
2024-06-14 19:02:48 +0300

    Init project

---
+++ src/main/java/com/colvir/bootcamp/salary/BootcampSalaryApplication.java
@@ -0,0 +1,13 @@
+package com.colvir.bootcamp.salary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSalaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampSalaryApplication.class, args);
+    }
+
+}
